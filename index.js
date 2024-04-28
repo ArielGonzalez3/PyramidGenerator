5d53b52d09ce6901574304f40e21f77a1c1e4f8b
ArielGonzalez3 <elvisarielgonzalez@gmail.com>
2024-04-28 23:44:00 +0200

    metodos de array

--- index.js
+++ index.js
@@ -31,15 +31,22 @@ const rows = [];
   rows.push(padRow(i, count));
 }*/
 
-let continueLoop = false;
-let done = 0;
 
-while(continueLoop) {
-  done++;
-  if(done == count) {
 
-  }
-}
+
+/*while(rows.length < count) {
+  
+  rows.push(padRow(rows.length + 1, count));
+  
+}*/
+
+/*for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
+}*/
+
+const numbers = [1, 2, 3];
+const unshifted = numbers.unshift(5)
+console.log(unshifted);
 
 let result = "";
 
